76f6be499b12cb94a828d7a21c7eee8dd6525379
Pahuts <luispahutan@gmail.com>
2018-09-03 20:37:08 +0800

    first commit

---
+++ app/src/main/java/ronaldo/pahutan/com/pahutanronaldolab1/MainActivity.java
@@ -0,0 +1,15 @@
+package ronaldo.pahutan.com.pahutanronaldolab1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout
+
+                .linear_layout);
+    }
+}
